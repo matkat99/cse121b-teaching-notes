52befd2a26714c6c5602e17f6d57bf2ff3cd29d5
matkat99 <st83440@gmail.com>
2020-10-05 11:38:43 -0600

    week 4

---
+++ sandbox/ui.js
@@ -0,0 +1,27 @@
+// wrapper function for querySelector
+export function qs(selector) {
+  return document.querySelector(selector);
+}
+
+export function setHtml(selector, text) {
+  qs(selector).innerHTML = text;
+}
+
+export function setListener(selector, event, callback) {
+  qs(selector).addEventListener(event, callback);
+}
+
+// create an alert at the top of the page for 3 seconds
+// requires the message to be displayed and the time in milliseconds.
+export function alertMessage(message, duration = 3000) {
+  const alert = document.createElement("p");
+  alert.innerHTML = message;
+  alert.setAttribute(
+    "style",
+    "background-color:lightpink; border: 1px solid red; position:absolute; top:0; left:0; right:0; padding: 1em;"
+  );
+  document.body.appendChild(alert);
+  setTimeout(function () {
+    document.body.removeChild(alert);
+  }, duration);
+}
